9f3e7cbfb3d81d70b2f2505992df5f31e5bdbf41
Evgenij00 <ganeevevgenij00@gmail.com>
2020-02-18 23:41:50 +0300

    Подключение gulp, sass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss'])
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['app/sass/**/*.sass', 'app/sass/**/*.scss'], gulp.parallel('sass'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'sass'));
