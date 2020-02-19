fbe14642d45c77f9f97ea7504af6a9f9d48fa9ea
Evgenij00 <ganeevevgenij00@gmail.com>
2020-02-19 19:59:08 +0300

    Псевдоклассы и импортирование данных

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ let gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss'])
+    return gulp.src(['app/sass/style.scss'])
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['app/sass/**/*.sass', 'app/sass/**/*.scss'], gulp.parallel('sass'));
+    gulp.watch(['app/sass/**/*.scss'], gulp.parallel('sass'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'sass'));
